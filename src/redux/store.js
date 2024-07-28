68494bc4d64ee6f9473e51d60d25810fa7331b02
bmbx08 <bmbx08@bu.ac.kr>
2024-07-29 00:08:07 +0900

    commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
-import { createStore, applyMiddleware } from "redux";
-import { thunk } from "redux-thunk";
-import rootReducer from "./reducers" //경로는 ./reducers/index 또는 ./reducers 만 써도 자동으로 index.js 읽어옴
+import {createStore, applyMiddleware} from "redux";
+import {composeWithDevTools} from "@redux-devtools/extension";
+import {thunk} from "redux-thunk";
+import rootReducer from "./reducers"; //경로는 ./reducers/index 또는 ./reducers 만 써도 자동으로 index.js 읽어옴
 
-let store= createStore(rootReducer,applyMiddleware(thunk));
+let store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
